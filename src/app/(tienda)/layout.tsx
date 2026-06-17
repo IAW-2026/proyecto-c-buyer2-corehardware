f9292c43eb6454b8c28edb3676cf81bd7c1a9079
Yanina Rivera <yanely299@gmail.com>
2026-06-17 20:56:40 -0300

    feat(layout): actualizar layout de tienda

--- src/app/(tienda)/layout.tsx
+++ src/app/(tienda)/layout.tsx
@@ -1,13 +1,16 @@
 import AppNavbar from '@/components/AppNavbar'
 import { Box, Flex } from '@chakra-ui/react'
+import { CartProvider } from '@/context/CartContext'
 
 export default function TiendaLayout({ children }: { children: React.ReactNode }) {
   return (
-    <Flex direction="column" minH="100vh">
-      <AppNavbar />
-      <Box flex={1} display="flex" flexDirection="column">
-        {children}
-      </Box>
-    </Flex>
+    <CartProvider>
+      <Flex direction="column" minH="100vh">
+        <AppNavbar />
+        <Box flex={1} display="flex" flexDirection="column">
+          {children}
+        </Box>
+      </Flex>
+    </CartProvider>
   )
 }
